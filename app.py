1e606eba2df16ad5cd206c4e7ef1afa56cb75409
arizaldiprasetya <61828586+arizaldiprasetya@users.noreply.github.com>
2024-12-10 13:27:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def index():
     }, 200
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=3000, debug=True, use_reloader=False)  # Disable reloader
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)  # Disable reloader
