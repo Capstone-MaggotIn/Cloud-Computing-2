421b78695787e962aeab73a34c8096daa1dd9993
arizaldiprasetya <arizaldi.prasetya04@gmail.com>
2024-12-10 12:50:46 +0700

    update konfigurasi database

--- app/services/storeData.py
+++ app/services/storeData.py
@@ -7,7 +7,7 @@ load_dotenv()
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
+        user=os.getenv('DB_USERNAME'),
         password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
     )
